c58c1709107a1877ab072f09a1bc77c10e7a2f75
andrewkozak91-boop <andrew.kozak91@gmail.com>
2025-10-10 12:25:33 -0400

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
-
-echo "🔧 Starting Render build process..."
+export PIP_ONLY_BINARY=:all:
+export PIP_NO_BUILD_ISOLATION=1
 python -V
 pip install --upgrade pip setuptools wheel
-pip install -r requirements.txt
+pip install --no-cache-dir -r requirements.txt
 echo "✅ Render build complete."
